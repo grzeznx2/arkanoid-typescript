bc903e3a79c063f23ad4900a59586f908800dc29
grzeznx2 <grzeznx2@gmail.com>
2022-01-11 12:01:37 +0100

    set winning / losing conditions

--- src/index.ts
+++ src/index.ts
@@ -60,6 +60,13 @@ function gameLoop(
     view.drawScore(score)
   }
 
+  // Game Over when ball leaves playField
+  if (ball.pos.y > view.canvas.height) gameOver = true
+  // If game won, set gameOver and display win
+  if (bricks.length === 0) return setGameWon(view)
+  // Return if gameover and don't run the requestAnimationFrame
+  if (gameOver) return setGameOver(view)
+
   requestAnimationFrame(() => gameLoop(view, bricks, paddle, ball, collision))
 }
 
