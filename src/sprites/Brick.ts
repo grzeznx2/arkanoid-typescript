e06abdf1fd82d2af91a90fa08f58f57bebb0b086
grzeznx2 <grzeznx2@gmail.com>
2022-01-11 09:51:37 +0100

    add Brick class properties, getters & setters

--- src/sprites/Brick.ts
+++ src/sprites/Brick.ts
@@ -1,3 +1,43 @@
 import { Vector } from '../types'
 
-export class Brick {}
+export class Brick {
+  private brickImage: HTMLImageElement = new Image()
+
+  constructor(
+    private brickWidth: number,
+    private brickHeight: number,
+    private position: Vector,
+    private brickEnergy: number,
+    image: string
+  ) {
+    this.brickWidth = brickWidth
+    this.brickHeight = brickHeight
+    this.brickEnergy = brickEnergy
+    this.position = position
+    this.brickImage.src = image
+  }
+
+  get width(): number {
+    return this.brickWidth
+  }
+
+  get height(): number {
+    return this.brickHeight
+  }
+
+  get pos(): Vector {
+    return this.position
+  }
+
+  get image(): HTMLImageElement {
+    return this.brickImage
+  }
+
+  get energy(): number {
+    return this.brickEnergy
+  }
+
+  set energy(value: number) {
+    this.brickEnergy = value
+  }
+}
