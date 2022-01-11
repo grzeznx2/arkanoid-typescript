08efe5613e2b33367ef9258760eaa18b06db6c56
grzeznx2 <grzeznx2@gmail.com>
2022-01-11 09:18:06 +0100

    add basic canvas methods

--- src/view/CanvasView.ts
+++ src/view/CanvasView.ts
@@ -16,4 +16,29 @@ export class CanvasView {
     this.start = document.querySelector('#start')
     this.info = document.querySelector('#info')
   }
+
+  clear(): void {
+    this.context?.clearRect(0, 0, this.canvas.width, this.canvas.height)
+  }
+
+  initStartButton(startFunction: (view: CanvasView) => void): void {
+    this.start?.addEventListener('click', () => startFunction(this))
+  }
+
+  drawScore(score: number): void {
+    if (this.scoreDisplay) this.scoreDisplay.innerHTML = score.toString()
+  }
+
+  drawInfo(text: string): void {
+    if (this.info) this.info.innerHTML = text
+  }
+
+  drawSprite(sprite: Ball | Paddle | Brick): void {
+    if (!sprite) return
+    this.context?.drawImage(sprite.image, sprite.pos.x, sprite.pos.y, sprite.width, sprite.height)
+  }
+
+  drawBricks(bricks: Brick[]): void {
+    bricks.forEach(this.drawSprite)
+  }
 }
